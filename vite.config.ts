0112595fbf19b8047b1260302401be095d469e1a
jhax <jhaxllc@gmail.com>
2025-05-31 20:42:43 -0400

    updated vite-config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MetaIntegrations/',
+  base: '/EmergentTech/',
   server: {
     host: '0.0.0.0', // Bind to all network interfaces
     port: 8080,      // Default Vite port
